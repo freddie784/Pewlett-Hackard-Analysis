d9da9f49123106319c9a3ac7fc0d30b4c4def920
freddie784 <83510059+freddie784@users.noreply.github.com>
2021-08-19 22:56:18 -0400

    added data to tables and cleaned some of the code

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -29,6 +29,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
+	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -52,11 +53,12 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 
 SELECT * FROM departments;
+
+
 
 
